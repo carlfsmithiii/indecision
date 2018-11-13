b1c7d20a545ffa7e2fd77048913ee12daea43847
Carl Smith <carlfsmithiii@gmail.com>
2018-11-13 16:47:19 -0500

    Add localStorage for options in src/app.js

--- src/app.js
+++ src/app.js
@@ -11,12 +11,36 @@ class IndecisionApp extends React.Component {
     };
   }
 
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem("options");
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (error) {
+      // do nothing
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length != this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem("options", json);
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
   handleDeleteOptions() {
     this.setState(() => ({ options: [] }));
   }
 
   handleDeleteOption(optionToRemove) {
-    this.setState((prevState) => ({
+    this.setState(prevState => ({
       options: prevState.options.filter(option => optionToRemove !== option)
     }));
   }
@@ -91,6 +115,7 @@ const Options = props => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && <p>Please add an option to get started</p>}
       {props.options.map(option => {
         return (
           <Option
@@ -108,7 +133,13 @@ const Option = props => {
   return (
     <div>
       {props.optionText}
-      <button onClick={() => {props.handleDeleteOption(props.optionText);}}>remove</button>
+      <button
+        onClick={() => {
+          props.handleDeleteOption(props.optionText);
+        }}
+      >
+        remove
+      </button>
     </div>
   );
 };
@@ -130,6 +161,10 @@ class AddOption extends React.Component {
     const error = this.props.handleAddOption(option);
 
     this.setState(() => ({ error }));
+
+    if (!error) {
+      event.target.elements.option.value = '';
+    }
   }
 
   render() {
