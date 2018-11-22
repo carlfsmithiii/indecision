82933e666c285349b7d6630bed9415bc1bdaa774
Carl Smith <carlfsmithiii@gmail.com>
2018-11-21 22:51:07 -0500

    install babel-core babel-loader

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import validator from 'validator';
-
-console.log(validator.isEmail('test'));
 
 // const template = <p>testing 123</p>;
 const template = React.createElement('p', {}, 'testing 123');
