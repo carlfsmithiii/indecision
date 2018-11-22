0e19373e42d62e2f83b2f3a7a857a250e14d13d5
Carl Smith <carlfsmithiii@gmail.com>
2018-11-21 23:05:12 -0500

    removed src/person.js and src/utils.js

--- public/bundle.js
+++ public/bundle.js
@@ -226,7 +226,7 @@ eval("\n\nif (false) {} else {\n  module.exports = __webpack_require__(/*! ./cjs
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-eval("\n\nvar _react = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\nvar _react2 = _interopRequireDefault(_react);\n\nvar _reactDom = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n\nvar _reactDom2 = _interopRequireDefault(_reactDom);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n// const template = <p>testing 123</p>;\nvar template = _react2.default.createElement('p', {}, 'testing 123');\n\n_reactDom2.default.render(template, document.getElementById('app'));\n\n//# sourceURL=webpack:///./src/app.js?");
+eval("\n\nvar _react = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\nvar _react2 = _interopRequireDefault(_react);\n\nvar _reactDom = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n\nvar _reactDom2 = _interopRequireDefault(_reactDom);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\nvar template = _react2.default.createElement(\n  'p',\n  null,\n  'THIS IS JSX FROM WEBPACK'\n);\n_reactDom2.default.render(template, document.getElementById('app'));\n\n//# sourceURL=webpack:///./src/app.js?");
 
 /***/ })
 
